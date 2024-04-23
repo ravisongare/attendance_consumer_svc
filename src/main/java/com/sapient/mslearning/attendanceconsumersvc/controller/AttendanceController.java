21f0e92abd51fd9db34d4fff8ae0c677bd9d0f9e
ravisongare <ravisongare2@gmail.com>
2024-04-23 20:33:27 +0530

    GraphQl Added.

--- src/main/java/com/sapient/mslearning/attendanceconsumersvc/controller/AttendanceController.java
+++ src/main/java/com/sapient/mslearning/attendanceconsumersvc/controller/AttendanceController.java
@@ -1,18 +1,14 @@
 package com.sapient.mslearning.attendanceconsumersvc.controller;
 
 import com.sapient.mslearning.attendanceconsumersvc.domain.Attendance;
-import com.sapient.mslearning.attendanceconsumersvc.entity.AttendanceEntity;
-import com.sapient.mslearning.attendanceconsumersvc.repository.AttendanceRepository;
 import com.sapient.mslearning.attendanceconsumersvc.service.AttendanceService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 public class AttendanceController {
@@ -27,15 +23,30 @@ public class AttendanceController {
         return "Hello World from consumer";
     }
 
-    @GetMapping("employee/{empId}")
-    ResponseEntity<Attendance> getEmployeeAttandance(@PathVariable("empId") int empId) {
-        logger.info("Inside getEmployeeAttandance with empId : {}", empId);
+//    @GetMapping("employee/{empId}")
+//    ResponseEntity<Attendance> getEmployeeAttandance(@PathVariable("empId") int empId) {
+//        logger.info("Inside getEmployeeAttandance with empId : {}", empId);
+//        Attendance attendance = service.getAttendanceDeatils(empId);
+//        Optional<Attendance> optionalAttendance = Optional.of(attendance);
+//        if (optionalAttendance.isPresent())
+//          //  return   optionalAttendance.get();
+//            return ResponseEntity.ok(optionalAttendance.get());
+//        else
+//            return ResponseEntity.notFound().build();
+//    }
+
+    @QueryMapping
+    Attendance getEmployeeAttendance(@Argument int empId) {
+        logger.info("Inside getEmployeeAttendance with empId : {}", empId);
         Attendance attendance = service.getAttendanceDeatils(empId);
-        Optional<Attendance> optionalAttendance = Optional.of(attendance);
-        if (optionalAttendance.isPresent())
-          //  return   optionalAttendance.get();
-            return ResponseEntity.ok(optionalAttendance.get());
-        else
-            return ResponseEntity.notFound().build();
+        return attendance;
     }
+    @QueryMapping
+    Attendance getDummyEmployeeAttendance(@Argument int empId) {
+        logger.info("Inside getEmployeeAttendance with empId : {}", empId);
+        Attendance attendance = new Attendance(1,"abs",null,60.0,"on bunk");
+
+        return attendance;
+    }
+
 }
